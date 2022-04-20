4098426e625d4c6403066688ec1868074f0334b7
skaur1027 <malhotra.sandeepk@gmail.com>
2022-04-20 10:14:53 -0700

    initial commit

---
+++ melonCounting.js
@@ -0,0 +1,24 @@
+let melonArray = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+function countMelons(melonList) {
+    let melonsCount = {};
+
+    for (const melon of melonArray) {
+        if (melonsCount[melon]) {
+            melonsCount[melon] += 1;
+        } else {
+            melonsCount[melon] = 1;
+        }
+    }
+    return melonsCount
+}
